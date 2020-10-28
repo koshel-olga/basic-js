e15eda4eaa8157610b2aae6de5e59b36a3e79253
moksyakova-olga <koshel.olga86@gmail.com>
2020-10-28 16:33:22 +0400

    feat: add function createDreamTeam and countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(searchCats) {
+  let cats = 0;
+  for (let i=0; i < searchCats.length; i++) {
+    for (let j=0; j < searchCats[i].length; j++){
+        if (searchCats[i][j] === '^^') { cats += 1}  
+    }
+  }
+  return cats;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(membersArr) {
+  let nameTeam = []
+  if (membersArr instanceof Array){
+    for (let member of membersArr) {
+      console.log(member)
+      if (typeof member == 'string') {
+        nameTeam.push(member.trim()[0].toUpperCase())
+        console.log(nameTeam)
+      }
+    }
+    return nameTeam.sort(). join('')
+  } else { return false}
 };
